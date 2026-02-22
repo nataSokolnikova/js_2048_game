82ffe31fc82382c671ff249725b253c9cd5e1d58
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2026-02-22 15:53:49 +0200

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -22,23 +22,157 @@ class Game {
    */
   constructor(initialState) {
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.size = 4;
+    this.score = 0;
+    this.status = 'idle'; // 'idle', 'playing', 'win', 'lose'
+
+    this.field = Array.from({ length: this.size }, () =>
+      Array(this.size).fill(0));
+    // console.log(initialState);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  addRandomTile() {
+    const empty = [];
+
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (this.field[row][col] === 0) {
+          empty.push([row, col]);
+        }
+      }
+    }
+
+    if (empty.length === 0) {
+      return;
+    }
+
+    const [rowIndex, colIndex] =
+      empty[Math.floor(Math.random() * empty.length)];
+
+    this.field[rowIndex][colIndex] = Math.random() < 0.9 ? 2 : 4;
+  }
+
+  checkWin() {
+    for (const row of this.field) {
+      if (row.includes(2048)) {
+        this.status = 'win';
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  checkLose() {
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (this.field[row][col] === 0) {
+          return false;
+        }
+
+        if (
+          col < this.size - 1 &&
+          this.field[row][col] === this.field[row][col + 1]
+        ) {
+          return false;
+        }
+
+        if (
+          row < this.size - 1 &&
+          this.field[row][col] === this.field[row + 1][col]
+        ) {
+          return false;
+        }
+        this.status = 'lose';
+
+        return true;
+      }
+    }
+  }
+
+  moveLeft() {
+    let moved = false;
+
+    for (let row = 0; row < this.size; row++) {
+      let newRow = this.field[row].filter((v) => v !== 0);
+
+      for (let i = 0; i < newRow.length - 1; i++) {
+        if (newRow[i] === newRow[i + 1]) {
+          newRow[i] *= 2;
+          this.score += newRow[i];
+          newRow[i + 1] = 0;
+        }
+      }
+      newRow = newRow.filter((v) => v !== 0);
+
+      while (newRow.length < this.size) {
+        newRow.push(0);
+      }
+
+      if (newRow.join() !== this.field[row].join()) {
+        moved = true;
+      }
+      this.field[row] = newRow;
+    }
+
+    if (moved) {
+      this.addRandomTile();
+    }
+    this.checkWin();
+    this.checkLose();
+  }
+  moveRight() {
+    this.field = this.field.map((row) => row.reverse());
+    this.moveLeft();
+    this.field = this.field.map((row) => row.reverse());
+  }
+  moveUp() {
+    this.rotateLeft();
+    this.moveLeft();
+    this.rotateRight();
+  }
+  moveDown() {
+    this.rotateLeft();
+    this.moveRight();
+    this.rotateRight();
+  }
+
+  rotateLeft() {
+    const newField = Array.from({ length: this.size }, () => Array(this.size).fill(0));
+
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        newField[this.size - col - 1][row] = this.field[row][col];
+      }
+    }
+    this.field = newField;
+  }
+
+  rotateRight() {
+    const newField = Array.from({ length: this.size }, () => Array(this.size).fill(0));
+
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        newField[col][this.size - row - 1] = this.field[row][col];
+      }
+    }
+    this.field = newField;
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.field.map((row) => row.slice());
+  }
 
   /**
    * Returns the current game status.
@@ -50,17 +184,28 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.score = 0;
+    this.status = 'playing';
+
+    this.field = Array.from({ length: this.size }, () => Array(this.size).fill(0));
+    this.addRandomTile();
+    this.addRandomTile();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.start();
+  }
 
   // Add your own methods here
 }
