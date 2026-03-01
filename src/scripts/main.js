21be86e3a106e143a9ae5f4c926753d96f3f9eeb
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2026-03-01 20:09:10 +0200

    final changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const Game = require('../modules/Game.class');
 const game = new Game();
 
 // Write your code here
-game.start();
+// game.start();
 
 const scoreEl = document.querySelector('.game-score');
 const messageStart = document.querySelector('.message-start');
@@ -57,6 +57,10 @@ function render() {
 
 // Клавіші для гри
 document.addEventListener('keydown', (e) => {
+  if (game.getStatus() !== 'playing') {
+    return; // гра не розпочата — игноруємо клавіші
+  }
+
   switch (e.key) {
     case 'ArrowLeft':
       game.moveLeft();
@@ -89,53 +93,3 @@ startButton.addEventListener('click', () => {
 
 // Початковий рендер
 render();
-
-// const updateUI = () => {
-//   const rows = document.querySelectorAll('.field-row');
-
-//   rows.forEach((tr, r) => {
-//     const cells = tr.querySelectorAll('.field-cell');
-
-//     cells.forEach((td, c) => {
-//       td.textContent = game.field[r][c] === 0 ? '' : game.field[r][c];
-//     });
-//   });
-//   document.querySelector('.game-score').textContent = game.getScore();
-
-//   document
-//     .querySelector('.message-lose')
-//     .classList.toggle('hidden', game.getStatus() !== 'lose');
-
-//   document
-//     .querySelector('.message-win')
-//     .classList.toggle('hidden', game.getStatus() !== 'win');
-// };
-
-// document.querySelector('.start').addEventListener('click', () => {
-//   game.start();
-//   updateUI();
-// });
-
-// document.addEventListener('keydown', (e) => {
-//   if (game.getStatus() !== 'playing') {
-//     return;
-//   }
-
-//   if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
-//     switch (e.key) {
-//       case 'ArrowUp':
-//         game.moveUp();
-//         break;
-//       case 'ArrowDown':
-//         game.moveDown();
-//         break;
-//       case 'ArrowLeft':
-//         game.moveLeft();
-//         break;
-//       case 'ArrowRight':
-//         game.moveRight();
-//         break;
-//     }
-//     updateUI();
-//   }
-// });
